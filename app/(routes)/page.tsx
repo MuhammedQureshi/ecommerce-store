bc3988421c991d35d0529448b7b3c93e8003282a
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-06-26 21:23:03 +0200

    feat: update bilboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("af747653-72c9-4df8-84f2-6b96bef00336");
+  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
 
   return (
     <Container>
