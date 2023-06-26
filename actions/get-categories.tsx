390fefb841ea9250ec5177bc70e32ea5f97477b1
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-06-26 03:36:31 +0200

    feat: fix fetch#

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -1,8 +1,8 @@
-import { Billboard } from "@/types";
+import { Category } from "@/types";
 
 const URL=`${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
-const getCategories = async (): Promise<Billboard[]> => {
+const getCategories = async (): Promise<Category[]> => {
   const res = await fetch(URL);
 
   return res.json();
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,19 +3,10 @@ import Link from "next/link";
 import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
 import NavbarActions from "@/components/navbar-actions";
-
-async function getData() {
-  const res = await fetch('http://localhost:3000/api/cfdf18cb-ae85-4d73-9928-1ecdee198f48/categories')
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
- 
-  return res.json()
-};
+import getCategories from "@/actions/get-categories";
 
 const Navbar = async () => {
-  const data = await getData();
+  const categories = await getCategories();
 
   return ( 
     <div className="border-b">
@@ -24,7 +15,7 @@ const Navbar = async () => {
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl">STORE</p>
           </Link>
-          <MainNav data={data} />
+          <MainNav data={categories} />
           <NavbarActions />
         </div>
       </Container>
